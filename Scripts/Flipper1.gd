44cdbbff6f644b04241261717c179ba88695a6ff
Alaa19971234 <95756868+Alaa19971234@users.noreply.github.com>
2023-03-10 14:46:15 -0500

    Add files via upload

--- Scripts/Flipper1.gd
+++ Scripts/Flipper1.gd
@@ -2,7 +2,7 @@ extends KinematicBody
 
 export var flipper_speed = 10  # adjust the speed of the flipper rotation
 
-var flipper_rot = 0  # keep track of the current flipper rotation
+var flipper_rot = -0.5  # keep track of the current flipper rotation
 
 func _ready():
 	pass  # add any initialization code here
@@ -11,7 +11,7 @@ func _input(event):
 		if event.is_action_pressed("flipper"):
 			flipper_rot = 2  # set the flipper rotation to a negative value to make it rotate upwards
 		elif event.is_action_released("flipper"):
-			flipper_rot = 0  # set the flipper rotation back to 0 to stop the rotation
+			flipper_rot = -0.5  # set the flipper rotation back to 0 to stop the rotation
 
 func _physics_process(delta):
 	# apply the flipper rotation to the flipper using the rotate_y function
--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -1,6 +1,28 @@
 extends RigidBody
 
+var collision_detected = false
+
+func _ready():
+	# Connect the body_entered signal to the ball
+	connect("body_entered", self, "_on_ball_body_entered")
+
+func _process(delta):
+	# After a collision with an object
+	if collision_detected:
+		# Check the ball's Y velocity
+		var y_velocity = get_linear_velocity().y
+		# Set a minimum Y velocity
+		var min_y_velocity = -500 # Change this to suit your game
+		# Clamp the Y velocity to the minimum value
+		var new_velocity = Vector3(get_linear_velocity().x, max(y_velocity, min_y_velocity), get_linear_velocity().z)
+		# Apply impulse to set the new velocity
+		apply_impulse(Vector3.ZERO, new_velocity - get_linear_velocity())
+		# Reset the collision_detected flag
+		collision_detected = false
+
 func _on_ball_body_entered(body):
 	if body.is_in_group("bumper"):
-		print("ball!");
+		print("ball!")
 		body.call("on_bumper_hit", self)
+	# Set the collision_detected flag
+	collision_detected = true
